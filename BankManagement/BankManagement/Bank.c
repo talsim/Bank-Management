bbba2d2e7a3c4020cc679e71dd960f60a6c1d65f
talsim <tals0606@gmail.com>
2018-07-14 13:18:11 +0300

    Start

--- BankManagement/BankManagement/Bank.c
+++ BankManagement/BankManagement/Bank.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+//void menu();
+//void new_acc();
+//void edit();
+//void transact();
+//void see();
+//void erase();
+//void quit();
 
-int main(int argc, char** args)
+static const char* new_acc_str = "bla\n";
+
+int main()
 {
-	printf("hello world\n");
+	printf("1. %s", new_acc_str); // new_acc
+
+	//show_menu();
+	//int option = get_user_option();
+	//exec_option(option);
+	//return 1;
 }
+//
+//void menu()
+//{
+//	int ans;
+//	printf("\t\t\t\t\tWelcome to the main menu\n\n\n");
+//	printf("1. %s", new_acc_str); // new_acc
+//	printf("2. Upadate information of existing account"); // edit
+//	printf("3. For trasactions"); // transact
+//	printf("4. Check the details of an existing account"); // see
+//	printf("5. Removing existing account"); // erase
+//	printf("6. Exit"); // quit
+//	scanf("%d", &ans);
+//	switch (ans)
+//	{
+//	case 1:
+//		new_acc();
+//		break;
+//	case 2:
+//		edit();
+//		break;
+//	case 3:
+//		transact();
+//		break;
+//	case 4:
+//		see();
+//		break;
+//	case 5: 
+//		erase();
+//	case 6: 
+//		quit();
+//	default:
+//		printf("Error: Invaild Input!");
+//	}
+//}
