fc1dfe3cb25491466f7027b01044a2668ab498a3
talsim <tals0606@gmail.com>
2018-07-21 13:47:57 +0300

    removed unnecessaryfunctions from bank api

--- BankManagement/BankManagement/bankAPI.c
+++ BankManagement/BankManagement/bankAPI.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "bankAPI.h"
+#include "userdata.h"
+
 FILE *userdata_file;
 Userdata* print_and_scan_new_acc_messages();
 void write_data_to_file(Userdata*);
@@ -107,23 +109,4 @@ static void write_data_to_file(Userdata *person)
 {
 	fprintf(userdata_file, write_format, person->name, person->age, person->id, person->phone_number, person->address);
 	fclose(userdata_file);
-}
-
-// fgets without reading newline character
-static char *read_line(char *str, int n, FILE *stream)
-{
-	char *ans = fgets(str, n, stream);
-	if (ans == NULL)
-		return NULL;
-	int newlineIndex = strcspn(str, "\n");
-	str[newlineIndex] = 0;
-	return str;
-}
-
-
-static int read_int(int *num)
-{
-	scanf("%d", num);
-	char c = getchar(); // remove newline from stdin
-	return *num;
 }
--- BankManagement/BankManagement/bankAPI.h
+++ BankManagement/BankManagement/bankAPI.h
@@ -1,21 +1,7 @@
-typedef struct
-{
-	char name[20];
-	int money;
-	int id;
-	int age;
-	int phone_number;
-	char address[20];
-} Userdata;
-
 void menu();
-int get_user_option();
-void exec_option(int);
 void new_acc();
 void edit();
 void transact();
 void see();
 void erase();
 void quit();
-char *read_line(char *str, int n, FILE *stream);
-int read_int(int *num);
